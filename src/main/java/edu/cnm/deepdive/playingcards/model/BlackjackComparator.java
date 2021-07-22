f77277447c067375e1d8597197bbd898e80442cc
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-07-22 09:57:51 -0600

    Completed blackjackComparator and blackjackhand subclass.

--- src/main/java/edu/cnm/deepdive/playingcards/model/BlackjackComparator.java
+++ src/main/java/edu/cnm/deepdive/playingcards/model/BlackjackComparator.java
@@ -2,61 +2,53 @@ package edu.cnm.deepdive.playingcards.model;
 
 import edu.cnm.deepdive.general.model.Hand;
 import java.util.Comparator;
-import java.util.Iterator;
+import java.util.Map;
 
 public class BlackjackComparator implements Comparator<Hand<Card>> {
 
-
-
+  private static final int HARD_SOFT_INCREMENT = 10;
+  private static final int BLACKJACK_HAND_SIZE = 2;
+  private static final int HAND_VALUE_LIMIT = 21;
+  private static final Map<Rank, Integer> RANK_VALUES = Map.ofEntries(
+      Map.entry(Rank.ACE, 1),
+      Map.entry(Rank.TWO, 2),
+      Map.entry(Rank.THREE, 3),
+      Map.entry(Rank.FOUR, 4),
+      Map.entry(Rank.FIVE, 5),
+      Map.entry(Rank.SIX, 6),
+      Map.entry(Rank.SEVEN, 7),
+      Map.entry(Rank.EIGHT, 8),
+      Map.entry(Rank.NINE, 9),
+      Map.entry(Rank.TEN, 10),
+      Map.entry(Rank.JACK, 10),
+      Map.entry(Rank.QUEEN, 10),
+      Map.entry(Rank.KING, 10)
+  );
 
   @Override
   public int compare(Hand<Card> hand1, Hand<Card> hand2) {
-
-    value(hand1);
-
-
-   /* *//*String one = hand1.toString();
-    String two = hand2.toString();
-    Integer oneI = Integer.valueOf(one);
-    Integer twoI = Integer.valueOf(two);*//*
-
-    return oneI.compareTo(twoI);*/
-
+    return value(hand1) - value(hand2);
   }
 
-  private int value (Hand<Card> hand1) {
-
-    Integer[] one = new Integer[hand1.size()];
-    int count = 0;
-
-    for (Card card : hand1) {
-    //  one[count] = Integer.valueOf(card.getRank());
-
-
+  protected int value(Hand<Card> hand) {
+    int total = 0;
+    boolean soft = false;
+    for (Card card : hand) {
+      total += RANK_VALUES.get(card.getRank());
+      if (card.getRank() == Rank.ACE && total <= HAND_VALUE_LIMIT - HARD_SOFT_INCREMENT) {
+        soft = true;
+        total += HARD_SOFT_INCREMENT;
+      } else if (total > HAND_VALUE_LIMIT && soft) {
+        soft = false;
+        total -= HARD_SOFT_INCREMENT;
+      }
     }
-
+    if (total > HAND_VALUE_LIMIT) {
+      total = -1;
+    } else if (total == HAND_VALUE_LIMIT && hand.size() == BLACKJACK_HAND_SIZE) {
+      total++;
+    }
+    return total;
   }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // TODO Implement (consider a helper method that computes a numeric value for a single
-    //      hand, then compare the 2 numeric values).
-    // Rules:
-    // No dealer (e.g. blackjack vs blackjack is a push)
-    // zero cards beats a bust.
-    // 2 card 21 beats a 3+ card 21.
-    // ace can be soft 11 or hard 1, in any hand only a single ace at most can be soft.
